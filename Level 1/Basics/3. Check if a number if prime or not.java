b0afe5f72f08c910937e2c3ded9fe5498e8c9e86
Vansh Ganesan <vllthag@gmail.com>
2026-05-09 15:33:26 +0530

    Update 3. Check if a number if prime or not.java

--- Level 1/Basics/3. Check if a number if prime or not.java
+++ Level 1/Basics/3. Check if a number if prime or not.java
@@ -138,3 +138,75 @@ class Main {
         }
     }
 }
+
+
+
+
+
+// Most optimal solution to this problem by breaking out of the loop right as we get a divisor
+
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of times that you would like to enter primes:");
+
+        int n;
+        int t;
+
+        while (true) {
+
+            t = sc.nextInt();
+
+            // t cant be greater than 10^4
+            if (t < 1 || t > 10000) {
+
+                System.out.println("The value you have entered exceeds all bounds, please enter the value again:");
+
+            } else {
+
+                break;
+            }
+        }
+
+        for (int i = 1; i <= t; i++) {
+
+            int count = 0;
+
+            System.out.println("Alright, now enter in the number to check if or not prime:");
+
+            while (true) {
+
+                n = sc.nextInt();
+
+                // n cant be greater then 10^9
+                if (n < 1 || n > 1000000000) {
+
+                    System.out.println("The value you have entered exceeds all bounds, please enter the value again:");
+
+                } else {
+
+                    break;
+                }
+            }
+
+            for (int j = 2; j * j <= n; j++) {
+
+                if (n % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+            
+            if (count == 0 && n != 1) {
+                System.out.println("The number is prime");
+            } else {
+                System.out.println("The number is not prime");
+            }
+        }
+    }
+}
