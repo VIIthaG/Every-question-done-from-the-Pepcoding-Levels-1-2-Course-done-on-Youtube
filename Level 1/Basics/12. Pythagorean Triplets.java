ebf92ea6816202afb8e580d64e26622cbf93bf3e
Vansh Ganesan <vllthag@gmail.com>
2026-05-18 20:44:43 +0530

    Update 12. Pythagorean Triplets.java

--- Level 1/Basics/12. Pythagorean Triplets.java
+++ Level 1/Basics/12. Pythagorean Triplets.java
@@ -1,96 +1,90 @@
-
 import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a,b,temp,c;
-        
-    while(true){
-        
+
+        int a, b, temp, c;
+
         while (true) {
 
-            System.out.println("Enter the  first of the 3 numbers that you wish to check make up a pythagorean triplet or no");
+            while (true) {
 
-            a = sc.nextInt();
-         
-            // upper limit is 10^9
-            if (a < 1 || a > 1000000000) {
+                System.out.println("Enter the  first of the 3 numbers that you wish to check make up a pythagorean triplet or no");
 
-                System.out.println("You have entered a value that is beyond the limit for one of the values, re-enter them:");
+                a = sc.nextInt();
 
-            } else {
-                
+                // upper limit is 10^9
+                if (a < 1 || a > 1000000000) {
 
-                break;
-            }
+                    System.out.println("You have entered a value that is beyond the limit for one of the values, re-enter them:");
 
-        }
+                } else {
 
-
-         while (true) {
-
-            System.out.println("Enter the  second of the 3 numbers that you wish to check make up a pythagorean triplet or no");
-
-            b = sc.nextInt();
-         
-            // upper limit is 10^9
-            if (  b < 1 || b > 1000000000 ) {
-
-                System.out.println("You have entered a value that is beyond the limit for one of the values, re-enter them:");
-
-            } else {
-                if(a>b){
-                    temp=a;
-                    a=b;
-                    b=temp;
+                    break;
                 }
-                
-                break;
+
             }
 
-        }
-        
-        
-        
-         while (true) {
+            while (true) {
 
-            System.out.println("Enter the  third of the 3 numbers that you wish to check make up a pythagorean triplet or no");
+                System.out.println("Enter the  second of the 3 numbers that you wish to check make up a pythagorean triplet or no");
 
-            c = sc.nextInt();
-         
-            // upper limit is 10^9
-            if ( c <1 || c >1000000000) {
+                b = sc.nextInt();
 
-                System.out.println("You have entered a value that is beyond the limit for one of the values, re-enter them:");
+                // upper limit is 10^9
+                if (b < 1 || b > 1000000000) {
 
-            } else {
-                 if(b>c){
-                    temp=b;
-                    b=c;
-                    c=temp;
+                    System.out.println("You have entered a value that is beyond the limit for one of the values, re-enter them:");
+
+                } else {
+
+                    if (a > b) {
+                        temp = a;
+                        a = b;
+                        b = temp;
+                    }
+
+                    break;
                 }
+
+            }
+
+            while (true) {
+
+                System.out.println("Enter the  third of the 3 numbers that you wish to check make up a pythagorean triplet or no");
+
+                c = sc.nextInt();
+
+                // upper limit is 10^9
+                if (c < 1 || c > 1000000000) {
+
+                    System.out.println("You have entered a value that is beyond the limit for one of the values, re-enter them:");
+
+                } else {
+
+                    if (b > c) {
+                        temp = b;
+                        b = c;
+                        c = temp;
+                    }
+
+                    break;
+                }
+
+            }
+
+            int lhs = (int) Math.pow(a, 2) + (int) Math.pow(b, 2);
+            int rhs = (int) Math.pow(c, 2);
+
+            if (lhs != rhs) {
+                System.out.println("The Numbers do not form a  Pythagorean triplet");
+            } else {
+                System.out.println("The Numbers do indeed form a  Pythagorean triplet");
                 break;
             }
 
         }
-        
-        int lhs= (int)Math.pow(a,2) + (int)Math.pow(b,2);
-        int rhs = (int)Math.pow(c,2);
-        
-      
-            
-        if(lhs!=rhs){
-            System.out.println("The Numbers do not form a  Pythagorean triplet");
-        }
-        else{
-            System.out.println("The Numbers do indeed form a  Pythagorean triplet");
-            break;
-            
-        }
-            
-        }
-
-       
 
     }
 }
