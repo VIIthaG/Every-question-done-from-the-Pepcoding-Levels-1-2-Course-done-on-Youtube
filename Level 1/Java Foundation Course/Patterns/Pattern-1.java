3f13bcce36b4481d31c6c181f84148870d026cdc
Vansh Ganesan <vllthag@gmail.com>
2026-05-21 13:42:25 +0530

    Update Pattern-1.java

--- Level 1/Java Foundation Course/Patterns/Pattern-1.java
+++ Level 1/Java Foundation Course/Patterns/Pattern-1.java
@@ -4,7 +4,7 @@ class Main {
         for (int i = 1; i <= 5; i++) {
 
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("* \t");
             }
 
             System.out.println("");
