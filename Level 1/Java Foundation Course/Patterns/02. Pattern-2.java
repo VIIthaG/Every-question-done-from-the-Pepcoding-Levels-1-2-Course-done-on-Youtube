fbe05f5fdeb09b51443b5b7e50073c18958c6129
Vansh Ganesan <vllthag@gmail.com>
2026-05-21 13:54:21 +0530

    Create 02. Pattern-2.java

---
+++ Level 1/Java Foundation Course/Patterns/02. Pattern-2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n;
+        
+        System.out.println("Enter a Number for the number of rows that you wish to print:");
+
+        while (true) {
+
+            n = sc.nextInt();
+
+            if (n < 1 || n > 100) {
+
+                System.out.println("Enter a valid number");
+
+            } else {
+
+                for (int i = n; i >= 1; i--) {
+
+                    for (int j = 1; j <= i; j++) {
+                        System.out.print("* \t");
+                    }
+
+                    System.out.println("");
+                }
+
+                break;
+            }
+        }
+    }
+}
