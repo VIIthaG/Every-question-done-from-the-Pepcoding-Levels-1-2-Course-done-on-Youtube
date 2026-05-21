aebaf6106807cc400a4e50dac6a4411434a04117
Vansh Ganesan <vllthag@gmail.com>
2026-05-21 17:17:00 +0530

    Create 07. Pattern-7.java

---
+++ Level 1/Java Foundation Course/Patterns/07. Pattern-7.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        System.out.println(
+            "Enter a Number for the number of rows that you wish to print:"
+        );
+
+        while (true) {
+
+            n = sc.nextInt();
+            int z = n;
+
+            if (n < 1 || n > 100 ) {
+
+                System.out.println("Enter a valid number");
+
+            } else {
+
+                for (int i = 1; i <= n; i++) {
+
+                    for (int j = 1; j < i; j++) {
+                        System.out.print("\t");
+                    }
+
+                    System.out.println("*\t");
+                }
+
+                break;
+            }
+        }
+    }
+}
