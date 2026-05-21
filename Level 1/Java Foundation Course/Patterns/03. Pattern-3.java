bf61ef333361429082c457151a579c282a97eb5a
Vansh Ganesan <vllthag@gmail.com>
2026-05-21 14:09:23 +0530

    Create 03. Pattern-3.java

---
+++ Level 1/Java Foundation Course/Patterns/03. Pattern-3.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n;
+        
+        System.out.println("Enter a Number for the number of rows that you wish to print:");
+
+        while (true) {
+
+            n = sc.nextInt();
+
+            if (n < 1 || n > 100) {
+
+                System.out.println("Enter a valid number");
+
+            } else {
+
+                for (int i = 1; i <=n ; i++) {
+                    
+                    for(int j=1; j<=n-i;j++){
+                        System.out.print("\t");
+                    }
+                    
+                    for (int k = 1; k <= i; k++) {
+                        System.out.print("* \t");
+                    }
+
+                    System.out.println("");
+                }
+
+                break;
+            }
+        }
+    }
+}
