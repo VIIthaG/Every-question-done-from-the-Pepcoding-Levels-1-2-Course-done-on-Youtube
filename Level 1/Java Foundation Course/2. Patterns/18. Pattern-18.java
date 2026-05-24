9bb31a31288b30fa7251524089942173277d440c
Vansh Ganesan <vllthag@gmail.com>
2026-05-24 13:13:08 +0530

    Add Pattern-18 Java program for printing patterns

---
+++ Level 1/Java Foundation Course/2. Patterns/18. Pattern-18.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the Number of rows that you would like to print, Keep in mind the number of rows can only be odd:");
+        while(true){
+            int n = sc.nextInt();
+            if(n<1 || n>10){
+                System.out.println("Enter the number again:");
+            }
+            else{
+                for(int i=1; i <=n; i++){
+                    for( int j = 1; j<=n; j++){
+                        
+                    if(i>n/2){
+                        if( i==j || i==n || i==1 || i+j==n+1 || j<i && j+i>n+1)
+                        {
+                          System.out.print("*\t");
+                        }
+                        else{
+                            System.out.print("\t");
+                        }
+                    }else{
+                        if( i==j || i==n || i==1 || i+j==n+1)
+                        {
+                          System.out.print("*\t");
+                        }
+                        else{
+                            System.out.print("\t");
+                        }
+                        
+                    }         
+                }
+                    System.out.println("\t");     
+            }
+                break;
+            
+            }    
+        }
+        
+    }
+}
