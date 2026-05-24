9fab4b1adc0f4be9cd1373c0886f6d8ec592b0fc
Vansh Ganesan <vllthag@gmail.com>
2026-05-24 13:17:09 +0530

    Update 18. Pattern-18.java

--- Level 1/Java Foundation Course/2. Patterns/18. Pattern-18.java
+++ Level 1/Java Foundation Course/2. Patterns/18. Pattern-18.java
@@ -1,4 +1,4 @@
-
+// My method
 import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
@@ -40,5 +40,65 @@ class Main {
             }    
         }
         
+    }
+}
+
+
+//method that is done in the video
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows (only odd numbers between 1 and 10):");
+
+        while (true) {
+
+            int n = sc.nextInt();
+
+            if (n < 1 || n > 10 || n % 2 == 0) {
+                System.out.println("Enter the number again:");
+            }
+
+            else {
+
+                int st = n;
+                int sp = 0;
+
+                for (int i = 1; i <= n; i++) {
+
+                    for (int j = 1; j <= sp; j++) {
+                        System.out.print("\t");
+                    }
+
+                    for (int j = 1; j <= st; j++) {
+
+                        if (i > 1 && i <= n / 2 && j > 1 && j < st) {
+                            System.out.print("\t");
+                        }
+
+                        else {
+                            System.out.print("*\t");
+                        }
+                    }
+
+                    if (i <= n / 2) {
+                        sp++;
+                        st -= 2;
+                    }
+
+                    else {
+                        sp--;
+                        st += 2;
+                    }
+
+                    System.out.println();
+                }
+
+                break;
+            }
+        }
     }
 }
